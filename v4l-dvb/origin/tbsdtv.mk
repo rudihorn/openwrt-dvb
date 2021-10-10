89191a2ddefe3b6749bf5674a1eeac3cd345c71b
Rudi <dyn-git@rudi-horn.de>
2021-10-10 19:57:19 +0000

    update tol atest

--- v4l-dvb/origin/tbsdtv.mk
+++ v4l-dvb/origin/tbsdtv.mk
@@ -2,11 +2,11 @@ PKG_NAME := v4l-dvb-tbsdtv
 
 PKG_SOURCE_PROTO := git
 PKG_SOURCE_URL := https://github.com/tbsdtv/media_build.git
-PKG_SOURCE_VERSION := ad3104e066fe7b0a62f3afd4179c68e09c741a9c
-PKG_SOURCE_DATE := 2021-02-02
+PKG_SOURCE_VERSION := c6194cd34ce43f396378b49d256b5e7f339006c6
+PKG_SOURCE_DATE := 2021-10-10
 
 V4L_SOURCE_URL := https://github.com/tbsdtv/linux_media.git
-V4L_SOURCE_VERSION := 0bc8cfa5b40283ec1f075d690c94c192c375b051
+V4L_SOURCE_VERSION := 91b94c9ed5293cd54b03137d7e0038cf998aca6f
 V4L_REPO_NAME := linux-media-tbsdtv
 V4L_SOURCE_FILE := $(V4L_REPO_NAME)-$(PKG_SOURCE_DATE)-$(shell printf '%.8s' $(V4L_SOURCE_VERSION)).tar.bz2
 
